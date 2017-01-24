984b07f15b80fbcc8fa8600574a3aa1a2cbe1217
Olivia Ayala <olivia.ayala96@gmail.com>
2017-01-24 15:14:58 -0600

    Added notes and hello.exe and hello.cs for C# project. Will add scala hw later.

---
+++ Experiments.scala
@@ -0,0 +1,12 @@
+object Experiments {
+
+  def main (args : Array[String]) : Unit = {
+    println ("Hello world")
+
+     println ("Uncomment me for more")
+
+    
+     println ("Or you can use C-style comments")
+     
+  }
+}
---
+++ src/main/scala/csp/ch01/Expressions.scala
@@ -0,0 +1,33 @@
+package csp.ch01
+
+
+object Expressions {
+  
+  sealed trait Expr
+  case class CstI (n:Int)                       extends Expr
+  case class Prim (op:String, e1:Expr, e2:Expr) extends Expr
+
+  def eval (e:Expr) : Int = {
+    e match {
+      case CstI (n)           => n
+      case Prim ("+", e1, e2) => eval (e1) + eval (e2)
+      case Prim ("*", e1, e2) => eval (e1) * eval (e2)
+      case Prim ("-", e1, e2) => eval (e1) - eval (e2)
+      case Prim (  _,  _,  _) => throw new RuntimeException ("unknown primitive")
+    }
+  }
+
+  def evalm (e:Expr) : Int = {
+    e match {
+      case CstI (n)           => n
+      case Prim ("+", e1, e2) => evalm (e1) + evalm (e2)
+      case Prim ("*", e1, e2) => evalm (e1) * evalm (e2)
+      case Prim ("-", e1, e2) => {
+        val res = evalm (e1) - evalm (e2)
+        if (res < 0) 0 else res
+      }
+      case Prim (  _,  _,  _) => throw new RuntimeException ("unknown primitive")
+    }
+  }
+
+}
